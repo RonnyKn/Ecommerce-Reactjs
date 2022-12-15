47de5755787f8e670fd65625ccdc7f23855937a4
RonnyKn <ronnykurniawn@gmail.com>
2022-12-15 14:29:15 +0700

    adding assets & Navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,8 +1,29 @@
 import React from "react"
 import "./Navbar.css"
+import { BiSearch, BiHeart, BiCartAlt } from "react-icons/bi"
+import Logo from "../../assets/img/logo.png"
 
 const Navbar = () => {
-  return <div>Navbar</div>
+  return (
+    <nav className="navbar">
+      <div className="container navbar-container">
+        <div className="navbar-logo">
+          <img src={Logo} alt="Logo" />
+        </div>
+        <div className="navbar-menu">
+          <button>
+            <BiSearch />
+          </button>
+          <button>
+            <BiHeart />
+          </button>
+          <button>
+            <BiCartAlt />
+          </button>
+        </div>
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar
