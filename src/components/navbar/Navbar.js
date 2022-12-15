5c2371381b42e236103f1477e2511ade6f0c528c
RonnyKn <ronnykurniawn@gmail.com>
2022-12-15 20:40:42 +0700

    update Header UI & adding scroll function Navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,11 +1,28 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import "./Navbar.css"
 import { BiSearch, BiHeart, BiCartAlt } from "react-icons/bi"
 import Logo from "../../assets/img/logo.png"
 
 const Navbar = () => {
+  const [navState, setNavState] = useState(false)
+
+  const onNavScroll = () => {
+    if (window.scrollY > 10) {
+      setNavState(true)
+    } else {
+      setNavState(false)
+    }
+  }
+  useEffect(() => {
+    window.addEventListener("scroll", onNavScroll)
+
+    return () => {
+      window.removeEventListener("scroll", onNavScroll)
+    }
+  }, [])
+
   return (
-    <nav className="navbar">
+    <nav className={`navbar ${navState ? "nav-30" : "nav-0"}`}>
       <div className="container navbar-container">
         <div className="navbar-logo">
           <img src={Logo} alt="Logo" />
