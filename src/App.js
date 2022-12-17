f661c8b9d3dd096f7f66e861bf82aa12e06a76c9
RonnyKn <ronnykurniawn@gmail.com>
2022-12-17 21:14:14 +0700

    adding Popular Sales Section

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import "./App.css"
-import { headerapi } from "../src/assets/data/Data"
+import { headerapi, popularsales, highlight } from "../src/assets/data/Data"
 import Header from "./components/header/Header"
 import Navbar from "./components/navbar/Navbar"
+import PopularSales from "./components/popularSales/PopularSales"
 
 function App() {
   return (
     <>
       <Navbar />
       <Header headerapi={headerapi} />
+      <PopularSales popularsales={popularsales} highlight={highlight} />
     </>
   )
 }
--- src/assets/data/Data.js
+++ src/assets/data/Data.js
@@ -1,6 +1,6 @@
 import headerimg from "../img/hero.png"
 
-// import hightlightimg from "../assets/hightlightimg.png"
+import hightlightimg from "../img/hightlightimg.png"
 // import sneakershoe from "../assets/sneaker.png"
 
 import clip from "../video/clip.mp4"
@@ -8,9 +8,9 @@ import vcover1 from "../video/vcover1.png"
 import vcover2 from "../video/vcover2.png"
 import vcover3 from "../video/vcover3.png"
 
-// import psale1 from "../assets/nike-air-red.png"
-// import psale2 from "../assets/nike-adapt-bb.png"
-// import psale3 from "../assets/nike-adapt-bb-smart.png"
+import psale1 from "../img/nike-air-red.png"
+import psale2 from "../img/nike-adapt-bb.png"
+import psale3 from "../img/nike-adapt-bb-smart.png"
 
 // import product1 from "../assets/product1.png"
 // import product2 from "../assets/product2.png"
@@ -37,4 +37,49 @@ const headerapi = {
   ],
 }
 
-export { headerapi }
+const popularsales = {
+  title: "Popular Sales",
+  items: [
+    {
+      id: "0p0x1",
+      title: "Nike Addapt BB 2.0",
+      text: "MEN Running Shoes",
+      rating: "4.9",
+      btn: "Buy Now",
+      img: psale2,
+      price: "200",
+      color: "from-blue-600 to-blue-500",
+    },
+    {
+      id: "0p0x2",
+      title: "Nike Martine Rose",
+      text: "MEN Running Shoes",
+      rating: "4.5",
+      btn: "Buy Now",
+      img: psale1,
+      price: "200",
+      color: "from-red-500 to-rose-500",
+    },
+    {
+      id: "0p0x3",
+      title: "Nike Smart Shoe 2.0",
+      text: "MEN Running Shoes",
+      rating: "5+",
+      btn: "Buy Now",
+      img: psale3,
+      price: "200",
+      color: "from-violet-500 to-indigo-500",
+    },
+  ],
+}
+
+const highlight = {
+  heading: "HIGHLIGHTS",
+  title: "NIKE AIR WITH LIMITLESS CHOICES",
+  text: "Our Purpose is to move the world forward. We take action by building community, protecting our planet and increasing access to sport.",
+  btn: "Explore More",
+  url: "https://www.nike.com/launch/t/nocta-hot-step-black-gold",
+  img: hightlightimg,
+}
+
+export { headerapi, popularsales, highlight }
