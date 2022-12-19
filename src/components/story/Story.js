3a35ad1d7f131a1c8809c32fb4a929da54e2b909
RonnyKn <ronnykurniawn@gmail.com>
2022-12-19 16:23:26 +0700

    fix bugs

--- src/components/story/Story.js
+++ src/components/story/Story.js
@@ -3,8 +3,27 @@ import React from "react"
 import { AiFillHeart } from "react-icons/ai"
 import { BiTimeFive } from "react-icons/bi"
 import { BsFillPersonFill } from "react-icons/bs"
+// import { Splide, SplideSlide } from "@splidejs/react-splide"
 
 const Story = ({ stories: { title, news } }) => {
+  // const splideOptions = {
+  //   perPage: 4,
+  //   perMove: 1,
+  //   type: "loop",
+  //   rewind: true,
+  //   keyboard: "global",
+  //   gap: "1rem",
+  //   pagination: false,
+  //   padding: "2rem",
+  //   breakpoints: {
+  //     1200: { perPage: 3 },
+  //     991: { perPage: 2.3 },
+  //     768: { perPage: 2 },
+  //     500: { perPage: 1.3 },
+  //     425: { perPage: 1 },
+  //   },
+  // }
+
   return (
     <section className="story">
       <div className="container story-container">
