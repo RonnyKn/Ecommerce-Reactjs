d6d1ddb57bf26e9a2f9df4db2bc7812156907aa8
RonnyKn <ronnykurniawn@gmail.com>
2022-12-20 15:39:19 +0700

    styling slider

--- src/components/story/Story.js
+++ src/components/story/Story.js
@@ -3,26 +3,29 @@ import React from "react"
 import { AiFillHeart } from "react-icons/ai"
 import { BiTimeFive } from "react-icons/bi"
 import { BsFillPersonFill } from "react-icons/bs"
-// import { Splide, SplideSlide } from "@splidejs/react-splide"
+import { Splide, SplideSlide } from "@splidejs/react-splide"
+import "@splidejs/splide/css"
+import { truncate } from "lodash"
 
 const Story = ({ stories: { title, news } }) => {
-  // const splideOptions = {
-  //   perPage: 4,
-  //   perMove: 1,
-  //   type: "loop",
-  //   rewind: true,
-  //   keyboard: "global",
-  //   gap: "1rem",
-  //   pagination: false,
-  //   padding: "2rem",
-  //   breakpoints: {
-  //     1200: { perPage: 3 },
-  //     991: { perPage: 2.3 },
-  //     768: { perPage: 2 },
-  //     500: { perPage: 1.3 },
-  //     425: { perPage: 1 },
-  //   },
-  // }
+  const splideOptions = {
+    perPage: 4,
+    perMove: 1,
+    type: "loop",
+    rewind: true,
+    keyboard: "global",
+    gap: "1rem",
+    pagination: true,
+    padding: "2rem",
+    autoplay: 500,
+    breakpoints: {
+      1200: { perPage: 3 },
+      991: { perPage: 2.3 },
+      768: { perPage: 2 },
+      500: { perPage: 1.3 },
+      425: { perPage: 1 },
+    },
+  }
 
   return (
     <section className="story">
@@ -30,29 +33,31 @@ const Story = ({ stories: { title, news } }) => {
         <div className="story-title">
           <h1>{title}</h1>
         </div>
-        <div className="story-content">
+        <Splide options={splideOptions} className="story-content">
           {news?.map((value, idx) => (
-            <article className="content-data" key={idx}>
-              <img src={value?.img} alt={`img ${idx}`} />
-              <div className="content-subs">
-                <span>
-                  <AiFillHeart color="red" /> <h6>{value?.like} </h6>
-                </span>
-                <span>
-                  <BiTimeFive color="blue" /> <h6>{value?.time}</h6>
-                </span>
-                <span>
-                  <BsFillPersonFill color="violet" /> <h6>{value?.by}</h6>
-                </span>
+            <SplideSlide className="content-data" key={idx}>
+              <div className="content1">
+                <img src={value?.img} alt={`img ${idx}`} />
+                <div className="content-subs">
+                  <span>
+                    <AiFillHeart color="red" /> <h6>{value?.like} </h6>
+                  </span>
+                  <span>
+                    <BiTimeFive color="blue" /> <h6>{value?.time}</h6>
+                  </span>
+                  <span>
+                    <BsFillPersonFill color="violet" /> <h6>{value?.by}</h6>
+                  </span>
+                </div>
+                <h5>{value?.title}</h5>
+                <p>{truncate(value?.text, { length: 175 })}</p>
+                <a href={value?.url} target="_blank" rel="noreferrer">
+                  {value?.btn}
+                </a>
               </div>
-              <h5>{value?.title}</h5>
-              <p>{value?.text}</p>
-              <a href={value?.url} target="_blank" rel="noreferrer">
-                {value?.btn}
-              </a>
-            </article>
+            </SplideSlide>
           ))}
-        </div>
+        </Splide>
       </div>
     </section>
   )
