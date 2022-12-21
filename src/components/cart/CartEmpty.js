7e251e6809c8cece6ae9ae643466dbd5aec8311d
RonnyKn <ronnykurniawn@gmail.com>
2022-12-21 14:32:04 +0700

    adding CartEmpty

--- src/components/cart/CartEmpty.js
+++ src/components/cart/CartEmpty.js
@@ -1,7 +1,18 @@
 import React from "react"
-
+import EmptyIMG from "../../assets/img/emptybag.png"
+import { HiOutlineArrowLeft, HiScale } from "react-icons/hi"
 const CartEmpty = () => {
-  return <div>CartEmpty</div>
+  return (
+    <>
+      <div className="cartempty">
+        <img src={EmptyIMG} alt="empty bag" />
+        <button type="button">
+          <HiOutlineArrowLeft className="cartempty-icon" />
+          Back To Store!
+        </button>
+      </div>
+    </>
+  )
 }
 
 export default CartEmpty
