977aefa3f2db90205f12963e5dd6580ceec17263
RonnyKn <ronnykurniawn@gmail.com>
2022-12-21 14:07:16 +0700

    adding CartCount

--- src/components/cart/CartCount.js
+++ src/components/cart/CartCount.js
@@ -1,7 +1,22 @@
 import React from "react"
+import { AiOutlineClose } from "react-icons/ai"
+import { HiChevronDoubleLeft } from "react-icons/hi"
 
 const CartCount = () => {
-  return <div>CartCount</div>
+  return (
+    <>
+      <div className="count-container">
+        <div className="count-item">
+          <h4>
+            <HiChevronDoubleLeft /> Your Cart : <span>(2 items)</span>
+          </h4>
+        </div>
+        <div className="count-close">
+          <AiOutlineClose color="#fff" />
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default CartCount
